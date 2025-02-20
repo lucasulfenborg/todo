879d64bf426919c3011ab775e3ac3f8ea310e9f8
Lucas Ulfenborg <lucasulfenborg@gmail.com>
2025-02-20 17:22:02 +0100

    add tasks

--- OOP2025_a24lucul_b24emica_assignment2/src/assignment2/ToDo.java
+++ OOP2025_a24lucul_b24emica_assignment2/src/assignment2/ToDo.java
@@ -1,15 +1,43 @@
 package assignment2;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class ToDo {
-
+public class ToDo implements ActionListener {
+	
+	JButton newStudyTaskButton, newHomeTaskButton;
+	JFrame frame;
+	
 	public static void main(String[] args) {
-		//Create a GUI window
-		JFrame frame = new JFrame();
+		new ToDo();
+	}
+	
+	public ToDo(){ //GUI Constructor
 		
-		Task task = new HomeTask();
-		frame.add(task.getGuiComponent());
+
+		//Window container and button component
+		frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		
+		newStudyTaskButton = new JButton("New StudyTask");
+		newStudyTaskButton.addActionListener(this);
+		frame.add(newStudyTaskButton);
+
+		
+		newHomeTaskButton = new JButton("New HomeTask");
+		newHomeTaskButton.addActionListener(this);
+		frame.add(newHomeTaskButton);
+		
+		
+		
+		
+		//Example for creating a studytask
+		//Task studyTask = new StudyTask();
+		//frame.add(studytask.getGuiComponent());
 		
 		//Position, dimensions, visibility & make window closeable
 		frame.setBounds(100,100,600,400);
@@ -19,4 +47,20 @@ public class ToDo {
 		
 	}
 
+	//Actions of buttons
+	public void actionPerformed (ActionEvent whichButton) {
+		if (whichButton.getSource().equals(newStudyTaskButton)) {
+			System.out.println("It is Button A") ;
+			Task task = new StudyTask();
+			frame.add(task.getGuiComponent());
+			frame.revalidate();
+		}
+		if (whichButton.getSource().equals(newHomeTaskButton)) {
+			System.out.println("It is Button B") ;
+			Task task = new HomeTask();
+			frame.add(task.getGuiComponent());
+			frame.revalidate();
+		}
+	}
+	
 }
