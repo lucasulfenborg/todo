ae81f009c46e2cbe48f5b2fc57d7aa8099eefcf0
lucas <lucasulfenborg@gmail.com>
2025-02-21 10:12:14 +0100

    fixed remove button

--- OOP2025_a24lucul_b24emica_assignment2/src/todo/ToDo.java
+++ OOP2025_a24lucul_b24emica_assignment2/src/todo/ToDo.java
@@ -1,18 +1,17 @@
-package assignment2;
+package todo;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ToDo implements ActionListener, TaskListener{
+public class ToDo implements ActionListener{
 	
 	JButton newStudyTaskButton, newHomeTaskButton;
 	JFrame frame;
 	
 	public static void main(String[] args) {
 		
-		//Create an instance of the application and start it
 	    ToDo application = new ToDo();
 
 	    application.execute();
@@ -20,54 +19,47 @@ public class ToDo implements ActionListener, TaskListener{
 	}
 	
 	private void execute() {
-		//Create the window
+		// TODO Auto-generated method stub
+		//Window container and button component
 		frame = new JFrame();
-		
-		//Configuration of the window: Layout, title, dimensions/size, visibility, make it exitable
 		frame.setLayout(new FlowLayout());
 		frame.setTitle("ToDo");
-		frame.setBounds(100,100,600,400);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//Create buttons
 		newStudyTaskButton = new JButton("New StudyTask");
 		newStudyTaskButton.addActionListener(this);
 		frame.add(newStudyTaskButton);
 
+		
 		newHomeTaskButton = new JButton("New HomeTask");
 		newHomeTaskButton.addActionListener(this);
 		frame.add(newHomeTaskButton);
 		
+		
+		
+		
+		//Example for creating a studytask
+		//Task studyTask = new StudyTask();
+		//frame.add(studytask.getGuiComponent());
+		
+		//Position, dimensions, visibility & make window closeable
+		frame.setBounds(100,100,600,400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	//Actions of buttons
 	public void actionPerformed (ActionEvent whichButton) {
-		
 		if (whichButton.getSource().equals(newStudyTaskButton)) {
-			//Action of newStudyTaskButton on click (create a study task button)
 			Task task = new StudyTask();
 			frame.add(task.getGuiComponent());
-			//Listen on the remove button for clicks
-			task.setTaskListener(this);
 			frame.revalidate();
 		}
 		if (whichButton.getSource().equals(newHomeTaskButton)) {
-			//Action of newHomeTaskButton on click (create a home task button)
 			Task task = new HomeTask();
 			frame.add(task.getGuiComponent());
-			//Listen on the remove button for clicks
-			task.setTaskListener(this);
 			frame.revalidate();
 		}
 	}
 	
-
-	//Action for the remove button on click (remove the button)
-	@Override 
-	public void taskRemoved(Task t) {
-		frame.remove(t.getGuiComponent()); 
-        frame.revalidate(); 
-        frame.repaint();
-	}
-	}
+	
+}
